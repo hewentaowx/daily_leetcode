5799d04fba36cd64407885f20dbdd49377cd517c
SonderzzZ <hewentaowx@gmail.com>
2019-04-01 11:50:34 +0800

    [add]增加findMedianSortedArrays 查找中位数函数方法

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
   - [towSum](./algo/index.js)
   - [addTwoNumbers](./algo/index.js)
   - [lengthOfLongestSubstring](./algo/index.js)
+
+- 2019/3/30
+  - 休息
+
+- 2019/3/31
+  - 休息
+
+- 2019/4/1
+  - [findMedianSortedArrays](./algo/index.js)
--- algo/index.js
+++ algo/index.js
@@ -89,21 +89,59 @@ const addTwoNumbers = (l1, l2) => {
 
  /**
   * 解答：
-  * @param {string} str
+  * @param {string} s
   * @returns {number}
   */
-const lengthOfLongestSubstring = str => {
+const lengthOfLongestSubstring = s => {
   let maxLen = 0;
   let start = 0;
   let end = 0;
-  for (; end < str.length; end++) {
-    let indexOf = str.substring(start, end).indexOf(str.charAt(end));
+  for (; end < s.length; end++) {
+    let indexOf = s.substring(start, end).indexOf(s.charAt(end));
     if (indexOf !== -1) {
       maxLen = Math.max(maxLen, end - start);
       start += indexOf + 1;
     }
   }
   return Math.max(maxLen, end - start);
+  // const map = {};
+  //   let left = 0;
+  //   return s.split('').reduce((r, c, i) => {
+  //     left = map[c] >= left ? map[c] + 1 : left;
+  //     map[c] = i;
+  //     return Math.max(r, i - left + 1);
+  //   }, 0);
 };
 
 console.log(lengthOfLongestSubstring('abcabcbb'));
+
+/**
+ * 题目介绍：
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。假设 nums1 和 nums2 不会同时为空
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))
+ * 示例：
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 则中位数为 2.0
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 则中位数是 (2 + 3) / 2 = 2.5
+ */
+
+/**
+ * 解答：
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+const findMedianSortedArrays = (nums1, nums2) => {
+  let nums = [...nums1, ...nums2].sort((a, b) => a - b);
+  const length = nums.length;
+  if (length % 2 === 0) {
+    return (nums[length / 2 - 1] + nums[length / 2]) / 2;
+  } else {
+    return nums[Math.floor(length / 2)];
+  }
+};
+
+console.log(findMedianSortedArrays([1, 2, 3], [2]));
